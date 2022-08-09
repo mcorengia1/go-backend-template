2afbf6ff7dd07a573c75775490b28f2d8e3277a5
mauro <mcorengia1@gmail.com>
2022-08-09 15:36:20 -0300

    example code working

---
+++ Go/go.mod
@@ -0,0 +1,7 @@
+module github.com/mcorengia1/go-backend-template
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
+
+require github.com/rs/cors v1.8.2 // indirect
---
+++ Go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+
+	Handlers "github.com/mcorengia1/go-backend-template/src/handlers"
+)
+
+func handleRequest(r *mux.Router) {
+
+	getR := r.Methods(http.MethodGet).Subrouter()
+	getR.HandleFunc("/", Handlers.ExampleHandler)
+
+	c := cors.New(cors.Options{
+		//AllowCredentials: true,
+		//AllowedOrigins: []string{"http://yourdomain.com", "https://yourdomain.com", "http://www.yourdomain.com", "https://www.yourdomain.com"},
+	})
+
+	handler := c.Handler(r)
+	err := http.ListenAndServe(":8000", handler)
+	fmt.Println(err)
+}
+
+func main() {
+
+	router := mux.NewRouter()
+	handleRequest(router)
+
+}
